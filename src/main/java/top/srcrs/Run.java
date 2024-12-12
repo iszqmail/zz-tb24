e932753fc39c1f74e492cdbc507bd5d9ba6bc350
Chelsea <133759653+iszqmail@users.noreply.github.com>
2024-12-13 01:30:25 +0800

    Update Run.java

--- src/main/java/top/srcrs/Run.java
+++ src/main/java/top/srcrs/Run.java
@@ -212,7 +212,7 @@ public class Run {
                     String body = "kw=" + s + "&tbs=" + tbs + "&sign=" + Encryption.enCodeMd5("kw=" + rotation + "tbs=" + tbs + "tiebaclient!!!");
                     JSONObject post = new JSONObject();
                     post = Request.post(SIGN_URL, body);
-                    int randomTime = new Random().nextInt(1200) + 1300;
+                    int randomTime = new Random().nextInt(500) + 800;
                     LOGGER.info("等待 {} 毫秒", randomTime);
                     TimeUnit.MILLISECONDS.sleep(randomTime);
                     if ("0".equals(post.getString("error_code"))) {
