520b8d84fc8f6f7f48149bdd160fe145da0c5512
Chelsea <133759653+iszqmail@users.noreply.github.com>
2025-03-05 23:59:08 +0800

    Update Request.java

--- src/main/java/top/srcrs/util/Request.java
+++ src/main/java/top/srcrs/util/Request.java
@@ -138,8 +138,6 @@ public class Request {
     public static Boolean isTiebaNotExist(String name) throws Exception {
 
         String url = "https://tieba.baidu.com/f?ie=utf-8&kw=" + name + "&fr=search";
-        return false;
-        /** 
         RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
         HttpClient client = HttpClients.custom().setDefaultRequestConfig(defaultConfig).build();
         HttpGet request = new HttpGet(url);
@@ -166,7 +164,6 @@ public class Request {
             LOGGER.info("{} 存在",maskedname);
             return false;
         }
-        */
     }
 }
 
