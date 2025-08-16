af03d2f5802c173f2480d1705b522c325224e161
Chelsea <133759653+iszqmail@users.noreply.github.com>
2025-08-17 02:16:16 +0800

    Update Run.java

--- src/main/java/top/srcrs/Run.java
+++ src/main/java/top/srcrs/Run.java
@@ -187,6 +187,7 @@ public class Run {
             LOGGER.info("获取贴吧列表成功");
             JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("like_forum");
             followNum = jsonArray.size();
+            LOGGER.info("用户关注贴吧数量: {} 个贴吧", followNum);
             // 获取用户所有关注的贴吧
             for (Object array : jsonArray) {
                 String tiebaName = ((JSONObject) array).getString("forum_name");
